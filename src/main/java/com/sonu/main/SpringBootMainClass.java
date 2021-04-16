c041a11155c13e1dfd409ada4834b1bc35637613
rafig <rafi.gdk@gmail.com>
2021-04-16 22:05:18 +0530

    fourth

--- src/main/java/com/sonu/main/SpringBootMainClass.java
+++ src/main/java/com/sonu/main/SpringBootMainClass.java
@@ -3,6 +3,8 @@ package com.sonu.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,8 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.sonu.main.repository"})
 @ComponentScan(basePackages = {"com.sonu.main.service", "com.sonu.main.controller", "com.sonu.main.exception"})
 @EntityScan(basePackages = {"com.sonu.main.model"})
+public class SpringBootMainClass extends SpringBootServletInitializer {
 
-public class SpringBootMainClass {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootMainClass.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMainClass.class, args);
